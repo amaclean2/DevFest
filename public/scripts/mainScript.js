01c1e562a94287d07fe20e90dc49b6c722dd8ed1
amaclean <andrew.n.maclean@gmail.com>
2018-09-10 00:12:47 -0700

    right-menu

--- public/scripts/mainScript.js
+++ public/scripts/mainScript.js
@@ -1,3 +1,34 @@
 function openMenu() {
-	console.log('open');
+	let nav = document.querySelectorAll('nav')[0],
+		container = nav.parentElement;
+
+	if(!document.querySelectorAll('.nav-background').length) {
+
+		navBackground = document.createElement('DIV');
+		navBackground.classList.add('nav-background');
+		navBackground.addEventListener('click', closeWindow);
+		container.insertBefore(navBackground, nav);
+
+	} else {
+		navBackground = document.querySelectorAll('.nav-background')[0];
+	}
+	
+	nav.style.display = 'block';
+	nav.classList.add('right-menu');
+	navBackground.style.display = 'block';
+	navBackground.classList.remove('nav-background-closed');
+}
+
+function closeWindow() {
+	let nav = document.querySelectorAll('nav')[0],
+		navBackground = document.querySelectorAll('.nav-background')[0];
+
+	nav.classList.remove('right-menu');
+	navBackground.classList.add('nav-background-closed');
+	nav.style.display = 'none';
+
+	setTimeout(() => {
+		navBackground.style.display = 'none';
+		navBackground.classList.remove('nav-background-closed');
+	}, 300);
 }
