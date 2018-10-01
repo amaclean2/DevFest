c93413d06b2a0862ba37dcdea9c8b7e3ada80f9c
Andres Pineda <humb.amp123@gmail.com>
2018-09-30 23:18:52 -0700

    add speaker modal info

---
+++ public/scripts/speakers.js
@@ -0,0 +1,77 @@
+let VikramObj = {
+  picture: "vikram_tiwari.png",
+  name: "Vikram Tiwari",
+  title: "End to End Machine Learning",
+  bio: "Interested in learning machince learning from beginning to end? Join in this all day code lab and you'll learn by developing a ML model using TensorFlow, training and deploying it on Google Cloud and then finally use it to get inferences."
+};
+
+let GrantObj = {
+  picture: "grant_timmerman.jpg",
+  name: "Grant Timmerman",
+  title: "All About Apps Script",
+  bio: " Learn how to increase the power of your favorite Google apps — like Calendar, Docs, Drive, Gmail, Sheets, and Slides – with JavaScript in Google Cloud."
+};
+
+
+let AishwaryaObj = {
+  picture: "aishwarya_parasuram.webp",
+  name: "Aishwarya Parasuram",
+  title: "Achieving Full Autonomy - A Drive Through Waymo’s Self-Driving Technology",
+  bio: "This talk is aimed at addressing the meaning of full autonomy, and what it takes to develop a fully self-driving system end to end. It dives into the various subsystems that form a fully self driving system, such as building 3D maps, identifying and classifying various objects and perceiving their characteristics using sensors and cameras, predicting what other objects might do next using machine learning and determining what our reaction should be using speed and geometry constraints. This talk also presents the components of a good multi-layered testing framework which makes fully autonomous driving a safe and comfortable reality, including the digitization of the real-world driving using simulation tools. The last section of the talk presents some challenging real-world problems that engineers face while developing these systems, and gives the audience an opportunity to engage with the speaker on some ideas to solve them. "
+};
+
+let QueenieObj = {
+  picture: "queenie_ho.png",
+  name: "Queenie Ho",
+  title: "Optimizing component development with interactive live development",
+  bio: "A design system is an ever-growing and ever-evolving source of truth for the basic parts from which entire product experiences are built from. Companies like Uber and AirBnb have switched over to this process in order to create a more cohesive product experience for their users.\n This talk will focus on how at Starcity, we developed our design system by applying Interactive Live Development making the process of development and iteration faster and more efficient. In a short coding session you will be able to see how simple it is to bring a component to life, test it, and write documentation on how to use it."
+};
+
+let RebecaObj = {
+  picture: "rebeca_gallardo.png",
+  name: "Rebeca	Gallardo",
+  title: "Where is my movie? - Inside Netflix's media storage system",
+  bio: "It's not enough to set up an infrastructure to store movie assets in the cloud, it's also imperative to secure them, perform adequate backup and provide other bookkeeping tasks.\n This talk will focus on the design process that allowed the video encoding team at Netflix to apply the learnings it had in this area to develop a system that manages tens of petabytes of assets for both streaming and studio production teams in the company.\n We will discuss the challenges faced, the outcomes reached, and the lessons learned, and we'll give a sneak peak at some of our paths forward."
+};
+
+let FrancesObj = {
+  picture: "frances_coronel.png",
+  name: "Frances	Coronel",
+  title: "Progressive Web Apps 101",
+  bio: "This class walks you through the steps of transforming an existing website into a Progressive Web App from the bottom up.\n Together we’ll also explore the vast array of companies that have already benefited from the many enhancements PWAs offer and why they’re so successful in emerging markets."
+};
+
+let TerrenceObj = {
+  picture: "terrence_ryan.png",
+  name: "Terrence	Ryan",
+  title: "Virtual Machine, Container, or Serverless",
+  bio: "One of the first challenges of moving your workloads to the Cloud is figuring out which three major flavors of computing technology to use: virtual machines, containers, or what is being called serverless. Each of these tools has its niche, its hype, and its downside. This session will tour application scenarios, discuss how each technology would handle them, discuss peculiar workloads,  consider the tradeoffs,  and leave you with recommendations for which one to pick. "
+};
+
+let MeiyiObj = {
+  picture: "android.png",
+  name: "Meiyi	Li",
+  title: "Build Android and iOS Apps with Flutter",
+  bio: "When writing an app, we all want to reach as wide an audience as possible.\n One of the challenges this poses for application developers is finding a way to do this efficiently. We want to save development time and minimize the amount of code that is duplicated, while maintaining the performance of a native mobile application.\n Meet Flutter! Flutter is Google’s free mobile app SDK for crafting high-quality native interfaces on iOS and Android. Flutter works with existing code, is used by developers and organizations around the world, and is open source.\n In this session, Alice Li, a Software Engineer at Google, will introduce you to mobile development with Flutter and show you how easy it is to use the Flutter framework to very quickly build expressive UIs."
+};
+
+let CheneyObj = {
+  picture: "cheney_tsai.webp",
+  name: "Cheney	Tsai",
+  title: "Modern Web in the Real World",
+  bio: "While the web has provided increased capabilities, success for real-world sites require taking a measured approach. In this session, you'll hear about challenges that some companies are tackling like organizational complexity, legacy code, measurement, prioritization and more to provide better experiences to their web users and prepare for future needs and demand. You'll also learn how AMP, PWA, and high quality UX have played a role in shaping these modern consumer journeys."
+};
+
+let CarenObj = {
+  picture: "caren_chang.png",
+  name: "Caren	Chang",
+  title: "Leveling Up Your Experience as an Android Developer",
+  bio: "Great! Now that you know what Activities are and how to make network requests off Android’s main UI, how do you take your Android development to the next level?\n This will be an introductory talk for beginners looking to gain a broader understanding of all that the Android developer’s ecosystem has to offer. We will touch on advantages and disadvantages of topics such as : Kotlin, RxJava, image loading libraries, gradle options, adb commands, different ways to debug, and much more.\n By the end of the talk, attendees should have a better understanding on how to advance their Android skills and have a sense of topics that they would be interested in further exploring."
+};
+
+let ChloeObj = {
+  picture: "chloe_condon.png",
+  name: "Chloe	Condon",
+  title: "Logging, and Errors, and Metrics- Oh My!",
+  bio: "As engineers, we build pretty cool apps. Once users start using our cool apps… well, we run into the fun process of discovering errors. Keeping track of these issues can get messy, getting alerted is stressful, and measuring it can provide you with an overwhelming amount of information. So, how do we combine all these things to make our cool apps work even better than before? In this talk, we’ll dive into logging, errors, and metrics, and how to use them to create the ultimate superpower of OBSERVABILITY!"
+};
